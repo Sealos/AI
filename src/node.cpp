274a7b41916f5f2653ae6619a8916f6dc37c8969
Sealos <sdecolli@gmail.com>
2014-04-17 01:57:36 -0430

    Lista de sucesores

--- src/node.cpp
+++ src/node.cpp
@@ -1,8 +1,6 @@
 // Nodo de 15 puzzle
 #include "node.h"
 
-using namespace std;
-
 bool node::is_goal()
 {
 #ifdef X_64
@@ -10,4 +8,68 @@ bool node::is_goal()
 #else
 	return (this->val[0] == 0x12345678 && this->val[1] == 0x9ABCDEF0);
 #endif
+}
+
+#define MOV_ARRIBA	0
+#define MOV_ABAJO	1
+#define MOV_DER		2
+#define MOV_IZQ		3
+
+std::list<int> node::succ()
+{
+	std::list<int> l_moves;
+	switch(this->pos_cero)
+	{
+	case 0:
+		l_moves.push_front(MOV_ABAJO);
+		l_moves.push_front(MOV_DER);
+		break;
+	case 1:
+	case 2:
+		l_moves.push_front(MOV_ABAJO);
+		l_moves.push_front(MOV_DER);
+		l_moves.push_front(MOV_IZQ);
+		break;
+	case 3:
+		l_moves.push_front(MOV_ABAJO);
+		l_moves.push_front(MOV_IZQ);
+		break;
+	case 4:
+	case 8:
+		l_moves.push_front(MOV_ARRIBA);
+		l_moves.push_front(MOV_ABAJO);
+		l_moves.push_front(MOV_DER);
+		break;
+	case 5:
+	case 6:
+	case 9:
+	case 10:
+		l_moves.push_front(MOV_ARRIBA);
+		l_moves.push_front(MOV_ABAJO);
+		l_moves.push_front(MOV_DER);
+		l_moves.push_front(MOV_IZQ);
+		break;
+	case 7:
+	case 11:
+		l_moves.push_front(MOV_ARRIBA);
+		l_moves.push_front(MOV_ABAJO);
+		l_moves.push_front(MOV_IZQ);
+		break;
+	case 12:
+		l_moves.push_front(MOV_ARRIBA);
+		l_moves.push_front(MOV_DER);
+		break;
+	case 13:
+	case 14:
+		l_moves.push_front(MOV_ARRIBA);
+		l_moves.push_front(MOV_DER);
+		l_moves.push_front(MOV_IZQ);
+		break;
+	case 15:
+		l_moves.push_front(MOV_ARRIBA);
+		l_moves.push_front(MOV_IZQ);
+		break;
+	}
+
+	return l_moves;
 }
--- src/node.h
+++ src/node.h
@@ -1,50 +1,11 @@
 // Nodo de 15 puzzle
 #include <stdio.h>
 #include <cstdlib>
+#include "define.h"
+#include <list>
 
 #ifndef NODE_H
 #define NODE_H
-
-#ifdef X_64
-
-#define POS_0	0xF000000000000000
-#define POS_1	0x0F00000000000000
-#define POS_2	0x00F0000000000000
-#define POS_3	0x000F000000000000
-#define POS_4	0x0000F00000000000
-#define POS_5	0x00000F0000000000
-#define POS_6	0x000000F000000000
-#define POS_7	0x0000000F00000000
-#define POS_8	0x00000000F0000000
-#define POS_9	0x000000000F000000
-#define POS_10	0x0000000000F00000
-#define POS_11	0x00000000000F0000
-#define POS_12	0x000000000000F000
-#define POS_13	0x0000000000000F00
-#define POS_14	0x00000000000000F0
-#define POS_15	0x000000000000000F
-
-#else
-
-#define POS_0	0xF0000000
-#define POS_1	0x0F000000
-#define POS_2	0x00F00000
-#define POS_3	0x000F0000
-#define POS_4	0x0000F000
-#define POS_5	0x00000F00
-#define POS_6	0x000000F0
-#define POS_7	0x0000000F
-#define POS_8	0xF0000000
-#define POS_9	0x0F000000
-#define POS_10	0x00F00000
-#define POS_11	0x000F0000
-#define POS_12	0x0000F000
-#define POS_13	0x00000F00
-#define POS_14	0x000000F0
-#define POS_15	0x0000000F
-
-#endif
-
 
 /***
  * La información que se guarda en un
@@ -59,11 +20,12 @@
  * 		la raíz del arbol de busqueda
 */
 
-
 class node
 {
-private:
+public:
 	int8_t pos_cero;
+	node *padre;
+
 #ifdef X_64
 	unsigned long int val;
 #else
@@ -87,7 +49,7 @@ public:
 	 * del estado s y las acciones
 	 * correspondientes
 	 */
-	//list succ(s);
+	std::list<int> succ();
 
 	/***
 	 * Construye la raíz del arbol de
