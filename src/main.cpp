2a93bef7217ad5fbd26495ac2e35657f4a928019
Karen <karen@karen-Lenovo-G560.(none)>
2014-04-18 01:52:52 -0430

    Toma de valores y pasar al constructor nodo.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "define.h"
+
 
 int main(int argc, const char* argv[])
 {
-#ifdef X_64
-	printf( "ola 64 bits\n");
-#else
-	printf( "ola 32 bits\n");
-#endif
+	std::ifstream infile(argv[1]);
+	std::string line;
+	unsigned int representacion[2] = { 0x00000000, 0x00000000 };
+	
+	while (std::getline(infile, line))
+	{
+		std::istringstream iss(line);
+		int n;
+		int i = 15;
+		
+		while (iss >> n)
+		{
+			if (i > 7) {
+				printf("%X\n", representacion[0]);
+				representacion[0] = representacion[0] | n << 4*i ; 
+			} else {
+				representacion[1] = representacion[1] | n << 4*i ;
+			}
+			i--;
+		}
+		
+		//node::node(representacion[0], representacion[1])
+	}
+	
 }
