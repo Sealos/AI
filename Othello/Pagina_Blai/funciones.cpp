b7d25043293516811ac72e8fd2b21c0357c93770
TroiKM <karentroiano2009@gmail.com>
2014-05-13 22:53:07 -0430

    miniMax
    Creo que está completo...y compila...

--- Othello/Pagina_Blai/funciones.cpp
+++ Othello/Pagina_Blai/funciones.cpp
@@ -11,11 +11,36 @@
 
 #include "funciones.h"
 
-
-int miniMax(state_t s, int depth){
+int miniMax(state_t s, int depth, bool max){
 	if (s.terminal() || depth == 0)
-		return s.value()
-	vector<int> succ = s.get_succ(true);
+		return s.value();
 	
+	if (max) {
+		std::vector<int> succ = s.get_succ(false);
+		if(succ.empty()){
+			return miniMax(s, depth - 1, false);
+		} else {
+			int bestValue = _INF;
+			for(int i = 0; i < succ.size(); ++i) {
+				state_t new_s = s.move(false, succ[i]);
+				bestValue = std::max(bestValue, miniMax(new_s, depth - 1, false));
+			}
+			return bestValue;
+		}
+	} else {
+		std::vector<int> succ = s.get_succ(true);
+		if(succ.empty()){
+			return miniMax(s, depth - 1, true);
+		} else {
+			int bestValue = INF;
+			for(int i = 0; i < succ.size(); ++i) {
+				state_t new_s = s.move(true, succ[i]);
+				bestValue = std::min(bestValue, miniMax(new_s, depth - 1, true));
+			}
+			return bestValue;
+		}
+	}
+	
+	return 0;
 }
 
--- Othello/Pagina_Blai/funciones.h
+++ Othello/Pagina_Blai/funciones.h
@@ -11,6 +11,13 @@
 
 #include "othello_cut.h"
 #include <limits.h>
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+const int _INF = INT_MIN;
+const int INF = INT_MAX;
 
 /*Declaracion de funciones utilizadas en Funciones.c*/
-extern int miniMax(state_t s, int depth);
+extern int miniMax(state_t s, int depth, bool max);
